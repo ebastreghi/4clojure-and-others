97c1153134c7ecfd52a609a4f6fe02304e09d9fa
Edevar Bastreghi Filho <edevarfilho@gmail.com>
2022-01-10 07:18:00 -0300

    Empty exercises

--- project.clj
+++ project.clj
@@ -3,9 +3,10 @@
   :url ""
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [clj-http "0.5.7"]
                  [cheshire "4.0.4"]
                  [org.clojure/math.combinatorics "0.0.3"]]
+  :repl-options {:init-ns offline-4clojure.core}
   :main  offline-4clojure.core
   )
