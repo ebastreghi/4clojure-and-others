84fd1804f86165f7053a66f145d33c6113cfed1c
Edevar Bastreghi Filho <edevarfilho@gmail.com>
2022-01-20 18:01:19 -0300

    recur solution for 30

--- src/offline_4clojure/p30.clj
+++ src/offline_4clojure/p30.clj
@@ -5,9 +5,20 @@
 (ns offline-4clojure.p30
   (:use clojure.test))
 
-(def __
-;; your solution here
-)
+(defn next-is-equal? [elem coll]
+  (= elem (first coll)))
+
+(defn remove-consecutive [coll]
+  (loop [first-elem (first coll)
+         coll-rest (rest coll)
+         acc []]
+    (if (some? first-elem)
+      (recur (if (next-is-equal? first-elem coll-rest) first-elem (first coll-rest))
+             (rest coll-rest)
+             (if (= first-elem (last acc)) acc (conj acc first-elem)))
+      acc)))
+
+(defn __ [coll] (remove-consecutive coll))
 
 (defn -main []
   (are [soln] soln
@@ -15,3 +26,4 @@
 (= (__ [1 1 2 3 3 2 2 3]) '(1 2 3 2 3))
 (= (__ [[1 2] [1 2] [3 4] [1 2]]) '([1 2] [3 4] [1 2]))
 ))
+
